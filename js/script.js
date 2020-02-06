8b6c8d483ad705ef0bc9462f087713b906000496
Kamil Bogucki <boguckikamil@hotmail.com>
2020-02-06 13:53:55 +0100

    very alpha, but playable game

--- js/script.js
+++ js/script.js
@@ -13,5 +13,45 @@ else if(randomNumber == 2){
 else {computerMove = 'nożyce';
 }
 
+printMessage('Komputer wybrał: ' + computerMove);
 
-printMessage('Mój ruch to: ' + computerMove);
+/* Human */
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+    playerMove = 'papier';
+}
+
+else if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+/* Win condition */
+
+if(playerMove == computerMove) {
+    printMessage('Jest remis');
+}
+
+else if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  }
+
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+}
+
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}
+
+else printMessage('Przykro mi, przegrywasz!');
