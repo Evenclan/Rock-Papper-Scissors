8fae699ae63b09a1906985f4036a694f21b02142
Kamil Bogucki <boguckikamil@hotmail.com>
2020-02-06 18:54:09 +0100

    formatting v2

--- js/script.js
+++ js/script.js
@@ -1,56 +1,41 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 let computerMove = 'nieznany ruch';
 
-  if(randomNumber == 1){
-  computerMove = 'kamień';
-}
-  
-  else if(randomNumber == 2){
-  computerMove = 'papier';
-} 
-
-  else {computerMove = 'nożyce';
+if (randomNumber == 1) {
+    computerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
 }
 
-  printMessage('Komputer wybrał: ' + computerMove);
+printMessage('Komputer wybrał: ' + computerMove);
 
 // Choice
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 let playerMove = 'nieznany ruch';
 
-  if(playerInput == '1'){
-  playerMove = 'kamień';
-}
-
-  else if(playerInput == '2'){
-  playerMove = 'papier';
-}
-
-  else if(playerInput == '3'){
-  playerMove = 'nożyce';
+if (playerInput == '1') {
+    playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier';
+} else if (playerInput == '3') {
+    playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
 
 // win condition
 
-  if(playerMove == computerMove) {
-  printMessage('Jest remis');
-}
-
-  else if( computerMove == 'kamień' && playerMove == 'papier'){
-  printMessage('Ty wygrywasz!');
-}
-
-  else if( computerMove == 'papier' && playerMove == 'nożyce'){
-  printMessage('Ty wygrywasz!');
-}
-
-  else if( computerMove == 'nożyce' && playerMove == 'kamień'){
-  printMessage('Ty wygrywasz!');
-}
-
-  else {
-  printMessage('Przykro mi, przegrywasz!'); 
+if (playerMove == computerMove) {
+    printMessage('Jest remis');
+} else if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+} else {
+    printMessage('Przykro mi, przegrywasz!');
 }
